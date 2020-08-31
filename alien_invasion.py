c1b4faa136b3be7ef7389d6c52299a99417cc2ac
Igot Gorchinskyiy <morgan78@bk.ru>
2020-08-31 21:29:34 +0300

    The ship moving Up, Down, Left, Right

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,17 +11,16 @@ class AlienInvasion:
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
         self.settings = Settings()
-
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.ship = Ship(self.screen)
+
+        self.ship = Ship(self)
 
     def run_game(self):
         """Запуск основного цикла игры."""
-
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
         # При каждом проходе цикла перерисовывается экран.
             """Отслеживание событий клавиатуры и мыши"""
@@ -31,13 +30,37 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            # Переместить корабль вправо.
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
         # При каждом проходе цикла перерисовывается экран.
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-
         # Отображение последнего прорисованного экрана.
         pygame.display.flip()
 
--- ship.py
+++ ship.py
@@ -7,12 +7,35 @@ class Ship:
     def __init__(self, ai_game):
         """Инициализирует корабль и задает его начальную позицию."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
         # Загружает изображение корабля и получает прямоугольник.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         # Каждый новый корабль появляется у нижнего края
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+        # Флаг перемещения
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        """Обновляет позицию корабля с учетом флага."""
+        # Обновляет атрибут х, а не rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        elif self.moving_up and self.rect.y > 0:
+            self.y -= self.settings.ship_speed
+        elif self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+        # Обновление атрибута rect на основании rect.x и rect.y
+        self.rect.x = self.x
+        self.rect.y = self.y
 
     def blitme(self):
         """Рисует корабль в текущей позиции."""
